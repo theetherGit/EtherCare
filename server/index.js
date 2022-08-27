c9cf366de756367f2397e4c59cba76d01d6f881c
Shivam Meena <meenashivam9650@gmail.com>
2022-08-27 17:09:14 +0530

    Added Express

---
+++ server/index.js
@@ -0,0 +1,16 @@
+import http from 'http';
+import express from 'express';
+import { handler } from '../build/handler.js';
+
+const app = express();
+const server = http.createServer(app);
+
+// SvelteKit handlers
+app.use(handler);
+
+const port = process.env.PORT || 3000;
+
+
+server.listen(port, () => {
+    console.log(`Running on http://localhost:${port}`);
+});
